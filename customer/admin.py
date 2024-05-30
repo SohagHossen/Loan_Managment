1bbfba4e34fbee049c686a5796e94ca646b18a87
SohagHossen <sohaghossen.cse@gmail.com>
2024-05-31 00:31:27 +0600

    data show in admin panel using admin.py

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from  customer.models import customer_data
 
-# Register your models here.
+# Register your models h
+
+class customeradmin(admin.ModelAdmin):
+    list_display =("id","first_name","last_name","email")
+
+admin.site.register(customer_data,customeradmin)
+
+
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from dashboard.models import Desh_inf
 # Register your models here.
+class DeshInfAdmin(admin.ModelAdmin):
+    list_display = ('desh_name','desh_age')
+admin.site.register(Desh_inf,DeshInfAdmin)
